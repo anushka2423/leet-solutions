2422b275445bf074737f8582ed4da1f1bb87c8f4
Anushka Srivastava <120164939+anushka2423@users.noreply.github.com>
2026-04-26 02:12:41 +0530

    Time: 5 ms (68.68%) | Memory: 43.5 MB (44.73%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        for (int i = 0; i < citations.length / 2; i++) {
+            int temp = citations[i];
+            citations[i] = citations[citations.length - 1 - i];
+            citations[citations.length - 1 - i] = temp;
+        }
+        int h = 0;
+
+        for(int i = 0; i < citations.length; i++) {
+            if(citations[i] >= i+1) h++;
+            else break;
+        }
+
+        return h;
+    }
+}
