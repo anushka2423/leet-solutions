2b53876d4f629619cb4a794dcf9af9cdfb4c7439
Anushka Srivastava <120164939+anushka2423@users.noreply.github.com>
2026-05-31 16:02:16 +0530

    Time: 2287 ms (5.87%) | Memory: 47 MB (7.84%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        int[] ans = new int[1];
+        solve(root, targetSum, 0, ans, true);
+
+        return ans[0];
+    }
+
+    private void solve(TreeNode root, int targetSum, long sum, int[] ans, boolean flag) {
+        if(root == null) return;
+        if(sum+root.val == targetSum) {
+            System.out.println(root.val);
+            ans[0]++;
+        }
+
+        solve(root.left, targetSum, sum+root.val, ans, false);
+        solve(root.right, targetSum, sum+root.val, ans, false);
+
+        if(flag) {
+            solve(root.left, targetSum, 0, ans, true);
+            solve(root.right, targetSum, 0, ans, true);
+        }
+    }
+}
