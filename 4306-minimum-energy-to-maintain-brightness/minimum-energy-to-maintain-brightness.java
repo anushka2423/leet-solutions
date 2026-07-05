db7ea09680cedb10cb57c36d72291611eb512a85
Anushka Srivastava <120164939+anushka2423@users.noreply.github.com>
2026-07-05 13:40:55 +0530

    Time: 432 ms (5.04%) | Memory: 202.8 MB (8.71%) - LeetSync

---
+++ 4306-minimum-energy-to-maintain-brightness/minimum-energy-to-maintain-brightness.java
@@ -0,0 +1,32 @@
+class Solution {
+    public long minEnergy(int n, int brightness, int[][] intervals) {
+        int illuminatePos = (int)Math.ceil((double)brightness/3.0);
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        for(int[] interval : intervals) {
+            int start = interval[0];
+            int end = interval[1];
+
+            if(!list.isEmpty() && list.get(list.size()-1).get(1) >= start) {
+                int oldEnd = list.get(list.size()-1).get(1);
+                list.get(list.size()-1).set(1, Math.max(oldEnd, end));
+            }else {
+                list.add(new ArrayList<>(List.of(start, end)));
+            }
+        }
+
+        long activeTime = 0;
+        for(List<Integer> li : list) {
+            int start = li.get(0);
+            int end = li.get(1);
+
+            System.out.println(start+" "+end);
+
+            activeTime += (end-start+1);
+        }
+
+        return illuminatePos * activeTime;
+    }
+}
