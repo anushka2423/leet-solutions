c3ee8e42de7757da8c828ee3087ae90dece64cac
Anushka Srivastava <120164939+anushka2423@users.noreply.github.com>
2026-09-13 14:18:33 +0530

    Time: 31 ms (81.39%) | Memory: 60.6 MB (5.09%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -12,15 +12,15 @@ class Solution {
                 int curr = nums[start]+nums[end];
 
                 if(curr == -nums[i]){
-                    if(ans.size() != 0){
-                        List<Integer> forCompare = ans.get(ans.size()-1);
-                        if(forCompare.get(0) == nums[i] && forCompare.get(1) == nums[start] && forCompare.get(2) == nums[end]) {
-                            start++;
-                            end--;
+                    // if(ans.size() != 0){
+                    //     List<Integer> forCompare = ans.get(ans.size()-1);
+                    //     if(forCompare.get(0) == nums[i] && forCompare.get(1) == nums[start] && forCompare.get(2) == nums[end]) {
+                    //         start++;
+                    //         end--;
 
-                            continue;
-                        }
-                    }
+                    //         continue;
+                    //     }
+                    // }
                     List<Integer> list = new ArrayList<>();
                     list.add(nums[i]);
                     list.add(nums[start]);
@@ -28,7 +28,9 @@ class Solution {
 
                     ans.add(list);
                     start++;
+                    while(start < end && nums[start] == nums[start-1]) start++;
                     end--;
+                    while(start < end && nums[end] == nums[end+1]) end--;
                 }else if(curr > -nums[i]) {
                     end--;
                 }else {
